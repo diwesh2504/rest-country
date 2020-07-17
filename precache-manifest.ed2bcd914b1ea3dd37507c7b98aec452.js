cbd501ce7a35a48cd89608f9c1a63532f52b2595
diwesh2504 <66382963+diwesh2504@users.noreply.github.com>
2020-07-17 15:03:55 +0530

    Updates

---
+++ precache-manifest.ed2bcd914b1ea3dd37507c7b98aec452.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c64eb6f33bbe75823b7cee25fb3b3047",
+    "url": "/rest-country/index.html"
+  },
+  {
+    "revision": "efddd1aa41a1d994d668",
+    "url": "/rest-country/static/css/main.f3a2c388.chunk.css"
+  },
+  {
+    "revision": "611e319dd33c493d354c",
+    "url": "/rest-country/static/js/2.ccd12674.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rest-country/static/js/2.ccd12674.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efddd1aa41a1d994d668",
+    "url": "/rest-country/static/js/main.ba933c16.chunk.js"
+  },
+  {
+    "revision": "6442bf831e2b43490dad",
+    "url": "/rest-country/static/js/runtime-main.a85e3173.js"
+  }
+]);
